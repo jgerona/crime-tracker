d1e9112ae76e4e0537f6e1c6a0fbe5cc4e35da5b
lingeorge88 <lingeorge04@gmail.com>
2023-02-07 21:22:49 -0800

    updated Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
-fetch("http://api.zippopotam.us/us/wa/seattle")
-.then(function(response){
-    return response.json();
+var cityInput = document.getElementById("cityInput");
+var stateInput =document.getElementById("stateInput");
+var searchEl = document.getElementById("searchBtn");
+var currentCity = document.getElementById("currentCity");
+var zipcode;
+
+function getZipcode(state,city){
+    fetch("http://api.zippopotam.us/us/"+ state + "/" + city)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        zipcode = data.places[0]["post code"];
+        console.log(zipcode);
+        getCrimemain();
+    })
+}
+
+function getCrimemain(){
+    fetch("https://crime-data-by-zipcode-api.p.rapidapi.com/crime_data?zip="+ zipcode)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data.success);
+    })
+}
+
+function displayCrimemain(){
+    currentCity.innerHTML = 
+}
+searchEl.addEventListener("click",function(){
+    city = cityInput.value;
+    state = stateInput.value;
+    getZipcode(state, city);
 })
-.then(function(data){
-    console.log(data);
-})
 
-
-
-fetch("https://crime-data-by-zipcode-api.p.rapidapi.com/crime_data?zip=94109")
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
